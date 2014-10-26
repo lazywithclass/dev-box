071e9c8b1d14a9dc3b3ba46111b507a770fc463c
Alberto Zaccagni <montecristoh@gmail.com>
2014-10-26 17:16:32 +0000

    JavaScript and generic tools modules updated

--- modules/javascript/manifests/init.pp
+++ modules/javascript/manifests/init.pp
@@ -1,10 +1,14 @@
 class javascript {
-  exec{ 'fetch-nave':
+  exec { 'fetch-nave':
     command => '/usr/bin/wget -q https://raw.githubusercontent.com/isaacs/nave/master/nave.sh -O /home/vagrant/bin/nave.sh',
-    creates => '/home/vagrant/bin/nave.sh',
+    creates => '/home/vagrant/bin/nave.sh'
   }
-  file{ '/home/vagrant/bin/nave.sh':
-    mode => 0755,
-    require => Exec['fetch-nave'],
+  exec { 'make-nave-executable':	
+    command => "/bin/chmod 744 /home/vagrant/bin/nave.sh",
+    require => Exec['fetch-nave']
+  }
+  exec { 'install-nodejs-stable':
+    command => '/home/vagrant/bin/nave.sh usemain stable',
+    require => Exec['make-nave-executable']
   }
 }
---
+++ modules/tools/manifests/init.pp
@@ -0,0 +1,6 @@
+Package { ensure => 'installed' }
+class tools {
+  package {
+    ['curl']
+  }
+}
