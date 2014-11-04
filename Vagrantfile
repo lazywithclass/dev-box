74a1296d1192617a05f7642dc32b7291c6d98a7b
Alberto Zaccagni <montecristoh@gmail.com>
2014-11-04 23:36:49 +0000

    Working configuration

--- Vagrantfile
+++ Vagrantfile
@@ -1,21 +1,18 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-# remember to add ssh credentials
-# mv ~/.ssh ~/.ssh-old
-# git clone https://username:password@github.com/username/repository.git ~/.ssh
-
-Vagrant.configure("2") do |config|
-  config.vm.box = "precise64"
-  config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+Vagrant.configure('2') do |config|
+  config.vm.box = 'precise64'
+  config.vm.box_url = 'http://files.vagrantup.com/precise64.box'
+  config.vm.hostname = 'dagobah'
   
-  # config.vm.provider "virtualbox" do |v|
+  # config.vm.provider 'virtualbox' do |v|
   #   v.gui = true
   # end
 
   config.vm.provision :puppet do |puppet|
-    puppet.manifests_path = "manifests"
-    puppet.manifest_file = "main.pp"
-    puppet.options = ["--verbose"]
+    puppet.manifests_path = 'manifests'
+    puppet.manifest_file = 'main.pp'
+    # puppet.options = ['--verbose --debug']
   end
 end
--- manifests/main.pp
+++ manifests/main.pp
@@ -1,4 +1,11 @@
-Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin/node' ] }
+Exec { 
+  path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/', '/usr/local/bin' ],
+  timeout => 999999
+}
+
+exec { 'system-update':
+  command => 'apt-get update'
+}
 
 file { '/home/vagrant/bin':
   ensure => 'directory',
@@ -6,20 +13,23 @@ file { '/home/vagrant/bin':
   group => 'vagrant'
 }
 
-file { '/home/vagrant/workspace':
-  ensure => 'directory',
-  owner => 'vagrant',
-  group => 'vagrant'
+class system {
+  package { 
+    ['tree', 'mplayer', 'xubuntu-desktop', 'zsh']: 
+      ensure => 'installed',
+      require => Exec['system-update']
+  }
 }
 
 class nodejs {
-  exec { 'install-curl':
-    command => 'apt-get install curl --assume-yes'
+  package { 'curl':
+    ensure => 'installed',
+    require => Exec['system-update']
   }
   exec { 'get-nave':	
     command => 'wget https://raw.github.com/isaacs/nave/master/nave.sh -O /home/vagrant/bin/nave',
-    require => Exec['install-curl'],
-    creates => '/home/vagrant/bin/nave'
+    creates => '/home/vagrant/bin/nave',
+    require => Package['curl']
   }
   file { '/home/vagrant/bin/nave':
     mode => 744,
@@ -34,4 +44,43 @@ class nodejs {
   }
 }
 
+class emacs {
+  package { 'python-software-properties':
+    ensure => 'installed',
+    require => Exec['system-update']
+  }  
+  exec { 'use-the-ppa':
+    command => 'add-apt-repository ppa:ubuntu-elisp/ppa -y',
+    require => Package['python-software-properties'],
+    onlyif => 'whereis emacs && return 1'
+  }
+  exec { 'update-for-emacs':
+    command => 'apt-get update',
+    require => Exec['use-the-ppa'],
+    onlyif => 'whereis emacs && return 1'
+  }
+  package { 'emacs-snapshot':
+    ensure => 'installed',
+    require => Exec['update-for-emacs']
+  }
+}
+
+class workspace {
+  file { '/home/vagrant/workspace':
+    ensure => 'directory',
+    owner => 'vagrant',
+    group => 'vagrant'
+  }
+  exec { 'clone-dotfiles':
+    command => 'git clone git@github.com:lazywithclass/dotfiles.git /home/vagrant/workspace/dotfiles',
+    user => 'vagrant',
+    creates => '/home/vagrant/workspace/dotfiles',
+    require => File['/home/vagrant/workspace']
+  }
+}
+
+
+include system
 include nodejs
+include emacs
+include workspace
