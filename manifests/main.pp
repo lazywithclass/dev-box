88a7210b3e7d1bb9f277509025b1a9ac3e57bd7b
lazywithclass <montecristoh@gmail.com>
2015-02-04 13:13:31 +0000

    added docker.io

--- manifests/main.pp
+++ manifests/main.pp
@@ -27,7 +27,7 @@ class system {
     require => Exec['system-upgrade']
   }
   package { 
-    ['tree', 'mplayer', 'xubuntu-desktop', 'zsh', 'terminator', 'gimp', 'virtualbox-guest-dkms', 'virtualbox-guest-utils', 'virtualbox-guest-x11', 'vim', 'tmux', 'fortune-mod', 'texinfo', 'expect', 'htop', 'calibre', 'erlang', 'markdown']: 
+    ['tree', 'mplayer', 'xubuntu-desktop', 'zsh', 'terminator', 'gimp', 'virtualbox-guest-dkms', 'virtualbox-guest-utils', 'virtualbox-guest-x11', 'vim', 'tmux', 'fortune-mod', 'texinfo', 'expect', 'htop', 'calibre', 'erlang', 'markdown', 'docker.io']: 
       ensure => 'installed',
       require => Exec['system-update']
   }
